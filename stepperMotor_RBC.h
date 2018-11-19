305cd4ef0e826a465a81c57c43b913d9bba5cb96
redbasecap <redbasecap@users.noreply.github.com>
2018-11-19 20:04:38 +0100

    Revert "Revert "Update stepperMotor_RBC.h""
    This reverts commit 76127dd51da65ad7dd833380d0fe4b0b2b02beac.

--- stepperMotor_RBC.h
+++ stepperMotor_RBC.h
@@ -1,7 +1,22 @@
+/*
+  stepperMotor_RBC - Library for driving the 28BYJ-48 stepper with ULN2003 driver board.
+  Created by RedBaseCap, 19. Nov, 2018.
+  GPL-3.0 by redbasecap/28BYJ-48_Stepper_with_ULN2003_Driver_Libary
+  */
+  
+#ifndef stepperMotor_RBC
+#define stepperMotor_RBC
+#include "Arduino.h"
+
 class stepperMotor_RBC
 {
 	public:
-	Morse(int pin);
-    void dot();
-    void dash();
+	void waveDrive(int);
+	void fullStep(int);
+	void halfStep(int);
+	void clearInputs();
+	void blinkSeq ();
 }
+
+
+#endif
